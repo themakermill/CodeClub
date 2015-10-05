af698e0a7b292057a3dc4774818579c4e6b4f196
Tim Baggs <timbaggs@hotmail.com>
2015-10-05 14:37:34 -0400

    Initial Commit

---
+++ ProductsApiApp/ProductTester/ProductTester/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace ProductTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+        }
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:61482/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/products/1");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Product product = await response.Content.ReadAsAsync<Product> ();
+                    Console.WriteLine("{0}\t{1}\t${2}\t{3}", product.Id, product.Name, product.Price, product.Category);
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
---
+++ ProductsApiApp/ProductViewer/ProductViewer/ProductViewer.py
@@ -0,0 +1,30 @@
+﻿import urllib
+import urllib.request
+import json
+
+def getProduct(prodID):
+  
+  url_1='http://localhost:61482/'
+  url_2='api/products/' + str(prodID)	
+ 
+  request = urllib.request.Request(url_1 + url_2)
+  response = urllib.request.urlopen(request)
+  data = json.loads(response.read().decode('utf-8'))
+
+  print(data)
+  print (str(data['Name']))
+
+def getProducts():
+  
+  url_1='http://localhost:61482/'
+  url_2='api/products/'	
+ 
+  request = urllib.request.Request(url_1 + url_2)
+  response = urllib.request.urlopen(request)
+  data = json.loads(response.read().decode('utf-8'))
+
+  print(data)
+
+getProduct(1)
+getProducts()
+
