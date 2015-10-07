dd5d4b15092e59835c27b719d8cb3ec932b00bba
Tim Baggs <timbaggs@hotmail.com>
2015-10-07 10:42:31 -0400

    Updated to dynamically pull down city list from web api

--- ProductsApiApp/WeatherApp/MainPage.xaml.cs
+++ ProductsApiApp/WeatherApp/MainPage.xaml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -24,18 +26,47 @@ namespace WeatherApp
         public MainPage()
         {
             this.InitializeComponent();
+
+            loadWeather();
         }
 
         private async void getWeather_Click(object sender, RoutedEventArgs e)
         {
-            var selectedItem = (ListViewItem)cityList.SelectedItem;
-            
-            RootObject myWeather = await OpenWeatherMapProxy.GetWeather(selectedItem.Content.ToString());
+            if (cityList.SelectedIndex > -1)
+            {
+                string selectedItem = cityList.SelectedItem.ToString();
 
-            string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
+                RootObject myWeather = await OpenWeatherMapProxy.GetWeather(selectedItem);
 
-            weatherImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
-            weatherText.Text = myWeather.name + " - " + ((int)myWeather.main.temp).ToString() + " - " + myWeather.weather[0].description;
+                string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
+
+                weatherImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+                weatherText.Text = myWeather.name + " - " + ((int)myWeather.main.temp).ToString() + " - " + myWeather.weather[0].description;
+            }
+        }
+
+        private async void loadWeather()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:61482/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/citys");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string resp = await response.Content.ReadAsStringAsync();
+
+                    List<City> list = Newtonsoft.Json.JsonConvert.DeserializeObject<List<City>>(resp);
+
+                    foreach (City city in list)
+                    {
+                        cityList.Items.Add(city.Name + ", " + city.State); 
+                    }
+                }
+            }
         }
     }
 }
