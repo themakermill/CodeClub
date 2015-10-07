93fe286e0dd75a946512baa817d82028950ee2ad
Tim Baggs <timbaggs@hotmail.com>
2015-10-06 23:09:24 -0400

    Added weatherapp

---
+++ ProductsApiApp/ProductsApiApp/Controllers/CitysController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ProductsApiApp.Models;
+
+namespace ProductsApiApp.Controllers
+{
+    public class CitysController : ApiController
+    {
+        City[] cities = new City[]
+        {
+            new City { Id = 1, Name = "North Andover", State = "MA"},
+            new City { Id = 2, Name = "San Francisco", State = "CA"},
+            new City { Id = 3, Name = "Phoenix", State = "AZ"}
+        };
+
+        public IEnumerable<City> GetAllCities()
+        {
+            return cities;
+        }
+
+        public IHttpActionResult GetCity(int id)
+        {
+            var city = cities.FirstOrDefault((p) => p.Id == id);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return Ok(city);
+        }
+    }
+}
---
+++ ProductsApiApp/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+namespace WeatherApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void getWeather_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = (ListViewItem)cityList.SelectedItem;
+            
+            RootObject myWeather = await OpenWeatherMapProxy.GetWeather(selectedItem.Content.ToString());
+
+            string icon = String.Format("ms-appx:///Assets/Weather/{0}.png", myWeather.weather[0].icon);
+
+            weatherImage.Source = new BitmapImage(new Uri(icon, UriKind.Absolute));
+            weatherText.Text = myWeather.name + " - " + ((int)myWeather.main.temp).ToString() + " - " + myWeather.weather[0].description;
+        }
+    }
+}
